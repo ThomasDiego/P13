ab639c72b603151e8275302c4c06c591280ebc8b
Thomas Diego <thomasdiego98@gmail.com>
2023-09-29 00:52:17 +0200

    :sparkles: Add html for edit FirstName & LastName

--- src/Pages/Dashboard/index.jsx
+++ src/Pages/Dashboard/index.jsx
@@ -1,14 +1,42 @@
 import "./style.css";
 import Button from "../../components/Form/Button/index.jsx";
 import {AccountBalance} from "../../components/AccountBalance/index.jsx";
+import {useEffect, useState} from "react";
 export const Dashboard = () => {
+    const [isEditingName, setIsEditingName] = useState(false)
+    const [realUser, setRealUser] = useState({ firstname: "John", lastname: "Doe" })
+    const [userInput, setUserInput] = useState({ firstname: "", lastname: "" })
+    useEffect(() => {
+        setUserInput(realUser)
+    }, [realUser])
+
+    const handleEditUser = () => {
+        setRealUser(userInput)
+        setIsEditingName(false)
+    }
     return (
         <div className={"dashboard"}>
             <div className="dashboardContainer">
                 <div className="dashboardTitle">
-                    <h1>Welcome back<br/>Tony Jarvis!</h1>
+                    {isEditingName ?
+                        <>
+                            <h1>Welcome back</h1>
+                            <div className={"inlineInput"}>
+                                <input className={"inputEdit"} type="text" value={userInput.firstname} placeholder={"Firstname"} onChange={(e) => {setUserInput({...userInput, firstname: e.target.value})}}/>
+                                <input className={"inputEdit"} type="text" value={userInput.lastname} placeholder={"Lastname"} onChange={(e) => {setUserInput({...userInput, lastname: e.target.value})}}/>
+                            </div>
+                            <div className={"inlineButton"}>
+                                <Button width={"fit-content"} content={"Save"} handleButtonCallback={handleEditUser}/>
+                                <Button width={"fit-content"} content={"Cancel"} handleButtonCallback={()=>(setIsEditingName(false))}/>
+                            </div>
+                        </>
+                        :
+                        <>
+                            <h1>Welcome back<br/>{realUser.firstname} {realUser.lastname}!</h1>
+                            <Button width={"fit-content"} content={"Edit name"} handleButtonCallback={()=>(setIsEditingName(true))}/>
+                        </>
+                    }
                 </div>
-                <Button width={"fit-content"} content={"Edit name"} handleButtonCallback={(e) =>{console.log("Button Dashboard Clicked")}}/>
                 <div className={"accountList"}>
                     <AccountBalance title={"Checking"} amount={"10000"}/>
                     <AccountBalance title={"Savings"} amount={"10"}/>
